da4f76818cc5778690e41d856eb117ad7b801e90
telome <130504305+telome@users.noreply.github.com>
2026-06-30 18:38:28 +0400

    Update deploy/LZAvaxMigrationL2Spell.sol
    Co-authored-by: sunbreak1211 <129470872+sunbreak1211@users.noreply.github.com>

--- deploy/LZAvaxMigrationL2Spell.sol
+++ deploy/LZAvaxMigrationL2Spell.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity ^0.8.22;
 
-import { LZAvaxMigrationInit, OftActivation } from "./LZAvaxMigrationInit.sol";
-import { UlnConfig } from "./LZInit.sol";
+import { LZAvaxMigrationInit, OftActivation, UlnConfig } from "./LZAvaxMigrationInit.sol";
 
 /// @notice One-off L2 spell for the Avalanche half of the migration, delegatecalled by the (old)
 ///         L2GovernanceRelay.
